11865667dea8082c9e9f15437b508e7c9380c6fe
Rotbarte <125587202+Rotbarte@users.noreply.github.com>
2023-02-19 20:02:06 +0000

    Katerina Rotbarte 221RDB292

--- main.py
+++ main.py
@@ -19,13 +19,9 @@ def find_mismatch(text):
 
         if next in ")]}":
             # Process closing bracket, write your code here
-             if not  opening_brackets_stack:
+             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char,next):
                 return i+1
-            
-            if not are_matching:
-                return i+1
-           
-        opening_brackets_stack.pop()
+           opening_brackets_stack.pop()
 
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
