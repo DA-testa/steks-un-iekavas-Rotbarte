de4b7a7ce99eabfcbb092af48001f66b26ecaf49
Rotbarte <125587202+Rotbarte@users.noreply.github.com>
2023-02-19 19:42:20 +0000

    Katerina Rotbarte 221RDB292

--- main.py
+++ main.py
@@ -14,18 +14,28 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next,i))
+            
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+             If not  opening_brackets_stack:
+                return i+1
+            
+            If not are_matching:
+                return i+1
+           
+        opening_brackets_stack.pop()
+        if opening_brackets_stack:
+            return opening_brackets_stack[0].position
+            return"Success"
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-
+    print(mismatch)
 
 if __name__ == "__main__":
     main()
